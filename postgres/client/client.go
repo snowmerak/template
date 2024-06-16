d8df2e63f5023ffe3ed9e7d181b825779e83b069
snowmerak <snowmerak@outlook.com>
2024-06-16 16:04:43 +0900

    remake client

---
+++ postgres/client/client.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Client is a PostgreSQL client.
+type Client struct {
+	pool *pgxpool.Pool
+}
+
+// New creates a new client for the given connection string.
+// The connection string should be a PostgreSQL connection string.
+// New returns an error if it fails to create a new client.
+// The client will automatically close the connection pool when the context is done.
+func New(ctx context.Context, connString string) (*Client, error) {
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		return nil, fmt.Errorf("pgxpool.New: %w", err)
+	}
+
+	context.AfterFunc(ctx, func() {
+		pool.Close()
+	})
+
+	return &Client{pool: pool}, nil
+}
+
+// Close closes the client's database connection pool.
+// Close should be called when the client is no longer needed.
+// But it is not necessary to call Close after a call to New.
+// Because the context passed to New will close the connection pool when it is done.
+func (c *Client) Close() {
+	c.pool.Close()
+}
+
+// Write some methods here to interact with the database.
