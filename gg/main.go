acded9492f6f0847f95c2839324627603a7c9f7b
snowmerak <snowmerak@outlook.com>
2024-06-18 08:37:33 +0900

    add dependency and go mod tidy

--- gg/main.go
+++ gg/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/AlecAivazis/survey/v2"
 	"log"
 	"os"
 	"os/exec"
@@ -8,6 +9,14 @@ import (
 )
 
 func main() {
+	workspaceData, err := os.ReadFile("go.work")
+	if err != nil {
+		log.Println("Failed to read workspace data:", err)
+		log.Println("Please run this command in the root of your workspace")
+		log.Println("- go work init")
+		return
+	}
+
 	templateOptions := []string{
 		"hello",
 		"http-client",
@@ -39,6 +48,14 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
+		return
+	}
+
+	workspaceData = append(workspaceData, []byte("\nuse "+moduleName)...)
+	if err := os.WriteFile("go.work", workspaceData, 0644); err != nil {
+		log.Println("Failed to write workspace data:", err)
+		log.Println("Please write this line to your go.work file")
+		log.Println("- use " + moduleName)
 		return
 	}
 
