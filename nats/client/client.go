3305cb4b8e255fd146fe43efb39294802b28404d
snowmerak <snowmerak@outlook.com>
2024-06-18 22:48:16 +0900

    add encoder and decoder apply generic

--- nats/client/client.go
+++ nats/client/client.go
@@ -6,14 +6,16 @@ import (
 )
 
 // Client is a wrapper around the nats.Conn struct
-type Client struct {
-	conn *nats.Conn
+type Client[T any] struct {
+	conn    *nats.Conn
+	encoder func(T) ([]byte, error)
+	decoder func([]byte) (T, error)
 }
 
 // New creates a new NATS client
 // It returns an error if the client cannot be created
 // The client is automatically closed when the context is done
-func New(ctx context.Context) (*Client, error) {
+func New[T any](ctx context.Context, encoder func(T) ([]byte, error), decoder func([]byte) (T, error)) (*Client[T], error) {
 	conn, err := nats.Connect(nats.DefaultURL)
 	if err != nil {
 		return nil, err
@@ -23,22 +25,38 @@ func New(ctx context.Context) (*Client, error) {
 		conn.Close()
 	})
 
-	return &Client{conn: conn}, nil
+	return &Client[T]{
+		conn:    conn,
+		encoder: encoder,
+		decoder: decoder,
+	}, nil
 }
 
 // Publish publishes a message to a subject
-func (c *Client) Publish(subject string, data []byte) error {
+func (c *Client[T]) Publish(subject string, value T) error {
+	data, err := c.encoder(value)
+	if err != nil {
+		return err
+	}
+
 	return c.conn.Publish(subject, data)
 }
 
 // Subscribe subscribes to a subject
-func (c *Client) Subscribe(subject string, handler nats.MsgHandler) (*nats.Subscription, error) {
-	return c.conn.Subscribe(subject, handler)
+func (c *Client[T]) Subscribe(subject string, handler func(subject string, value T)) (*nats.Subscription, error) {
+	return c.conn.Subscribe(subject, func(msg *nats.Msg) {
+		value, err := c.decoder(msg.Data)
+		if err != nil {
+			return
+		}
+
+		handler(msg.Subject, value)
+	})
 }
 
 // Close closes the client
 // It should be called when the client is no longer needed
 // But it is automatically called when the context is done
-func (c *Client) Close() {
+func (c *Client[T]) Close() {
 	c.conn.Close()
 }
