d61e0702ef2678b165c716ad1717bf28d51455bb
snowmerak <snowmerak@outlook.com>
2024-06-18 09:00:22 +0900

    add init and clone option

--- gg/main.go
+++ gg/main.go
@@ -10,70 +10,111 @@ import (
 )
 
 func main() {
-	pwd, err := os.Getwd()
-	if err != nil {
-		log.Println("Failed to get current working directory:", err)
+	if len(os.Args) < 2 {
+		log.Println("Usage: gg <command>")
+		log.Println("Commands:")
+		log.Println("  init: Initialize workspace")
+		log.Println("  clone: Clone a repository from template")
 		return
 	}
-	path := ""
 
-	workspaceData := make([]byte, 0)
-	for {
-		workspaceData, err = os.ReadFile(filepath.Join(pwd, "go.work"))
-		if err == nil {
-			break
-		}
-
-		path = filepath.Join(filepath.Base(pwd), path)
-		pwd = filepath.Dir(pwd)
-
-		if pwd == "/" || pwd == "" || strings.HasSuffix(pwd, ":\\") {
-			log.Println("Failed to find workspace file")
+	switch os.Args[1] {
+	case "init":
+		rootWorkspace := ""
+		if err := survey.AskOne(&survey.Input{
+			Message: "Enter root workspace repository",
+		}, &rootWorkspace); err != nil {
+			log.Println("Failed to get root workspace repository:", err)
 			return
 		}
-	}
 
-	templateOptions := []string{
-		"hello",
-		"http-client",
-		"http-server",
-		"mono",
-		"nats",
-		"postgres",
-		"redis",
-		"executable",
-	}
+		cmd := exec.Command("go", "work", "init")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			log.Println("Failed to initialize workspace:", err)
+			return
+		}
 
-	selectedTemplate := ""
-	if err := survey.AskOne(&survey.Select{
-		Message: "Select template",
-		Options: templateOptions,
-	}, &selectedTemplate); err != nil {
-		return
-	}
+		workspaceData, err := os.ReadFile("go.work")
+		if err != nil {
+			log.Println("Failed to read workspace data:", err)
+			return
+		}
 
-	moduleName := ""
-	if err := survey.AskOne(&survey.Input{
-		Message: "Enter module name",
-	}, &moduleName); err != nil {
-		return
-	}
+		workspaceData = append([]byte("// "+rootWorkspace+"\n"), workspaceData...)
+		if err := os.WriteFile("go.work", workspaceData, 0644); err != nil {
+			log.Println("Failed to write workspace data:", err)
+			return
+		}
+	case "clone":
+		pwd, err := os.Getwd()
+		if err != nil {
+			log.Println("Failed to get current working directory:", err)
+			return
+		}
+		path := ""
 
-	cmd := exec.Command("gonew", "github.com/snowmerak/template/"+strings.ReplaceAll(selectedTemplate, "-", ""), moduleName)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		return
-	}
+		workspaceData := make([]byte, 0)
+		for {
+			workspaceData, err = os.ReadFile(filepath.Join(pwd, "go.work"))
+			if err == nil {
+				break
+			}
 
-	modulePath := filepath.Join(path, filepath.Base(moduleName))
+			path = filepath.Join(filepath.Base(pwd), path)
+			pwd = filepath.Dir(pwd)
 
-	workspaceData = append(workspaceData, []byte("\nuse "+modulePath)...)
-	if err := os.WriteFile(filepath.Join(pwd, "go.work"), workspaceData, 0644); err != nil {
-		log.Println("Failed to write workspace data:", err)
-		log.Println("Please write this line to your go.work file")
-		log.Println("- use " + modulePath)
-		return
+			if pwd == "/" || pwd == "" || strings.HasSuffix(pwd, ":\\") {
+				log.Println("Failed to find workspace file")
+				return
+			}
+		}
+
+		rootWorkspace := strings.TrimPrefix(strings.SplitN(string(workspaceData), "\n", 2)[0], "// ")
+
+		templateOptions := []string{
+			"hello",
+			"http-client",
+			"http-server",
+			"mono",
+			"nats",
+			"postgres",
+			"redis",
+			"executable",
+		}
+
+		selectedTemplate := ""
+		if err := survey.AskOne(&survey.Select{
+			Message: "Select template",
+			Options: templateOptions,
+		}, &selectedTemplate); err != nil {
+			return
+		}
+
+		moduleName := ""
+		if err := survey.AskOne(&survey.Input{
+			Message: "Enter module name",
+		}, &moduleName); err != nil {
+			return
+		}
+
+		cmd := exec.Command("gonew", "github.com/snowmerak/template/"+strings.ReplaceAll(selectedTemplate, "-", ""), filepath.ToSlash(filepath.Join(rootWorkspace, moduleName)))
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return
+		}
+
+		modulePath := filepath.Join(path, filepath.Base(moduleName))
+
+		workspaceData = append(workspaceData, []byte("\nuse "+modulePath)...)
+		if err := os.WriteFile(filepath.Join(pwd, "go.work"), workspaceData, 0644); err != nil {
+			log.Println("Failed to write workspace data:", err)
+			log.Println("Please write this line to your go.work file")
+			log.Println("- use " + modulePath)
+			return
+		}
 	}
 
 	log.Println("Done")
