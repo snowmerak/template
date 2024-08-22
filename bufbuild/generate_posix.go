b7ee0a70fc1b2114dc8b044ea87b39e68aae7192
snowmerak <snowmerak@outlook.com>
2024-08-22 22:59:18 +0900

    replace pwd with .

--- bufbuild/generate_posix.go
+++ bufbuild/generate_posix.go
@@ -3,4 +3,4 @@
 package bufbuild
 
 // when you are working on linux or macos
-//go:generate docker run -rm --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf generate
+//go:generate docker run -rm --volume ".:/workspace" --workdir /workspace bufbuild/buf generate
--- postgres/generate_posix.go
+++ postgres/generate_posix.go
@@ -3,4 +3,4 @@
 package postgres
 
 // when you are working on linux or macos
-//go:generate docker run --rm -v $(pwd):/src -w /src sqlc/sqlc generate
+//go:generate docker run --rm -v .:/src -w /src sqlc/sqlc generate
